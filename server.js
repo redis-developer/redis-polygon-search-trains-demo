3d4e2dd1b5b45c49d2d2903c93140597b1005fc1
Simon Prickett <simon@redislabs.com>
2023-08-11 23:11:11 +0100

    Added skeleton search route.

--- server.js
+++ server.js
@@ -24,8 +24,14 @@ app.get('/', async (req, res) => {
   return res.render('homepage');
 });
 
-// Example query:
-// FT.SEARCH idx:stations "@position:[within $poly]" RETURN 1 name PARAMS 2 poly "POLYGON((-122.387096 37.724491, -122.360487 38.802250, -122.521058 37.800800, -122.505826 37.705039, -122.387096 37.724491))" DIALECT 3
+// Perform a polygon search and return the results.
+app.post('/search', async (req, res) => {
+  // Example query:
+  // FT.SEARCH idx:stations "@position:[within $poly]" RETURN 1 name PARAMS 2 poly "POLYGON((-122.387096 37.724491, -122.360487 38.802250, -122.521058 37.800800, -122.505826 37.705039, -122.387096 37.724491))" DIALECT 3
+
+  // Results need to include station name, lat, lng.
+  return res.json({ 'search': 'TODO'});
+});
 
 // Start the Express server.
 app.listen(PORT, () => {
