2a15af67bb5577bca578ee37c60d65fc45151b02
Simon Prickett <simon@redislabs.com>
2023-08-11 20:09:49 +0100

    Added basic marker drops.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,23 @@
-console.log('js works');
 const myMap = L.map('mapid').setView([37.6570598, -122.2636107], 10);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+const redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+  {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }
+).addTo(myMap);
+
+myMap.on('click', (e) => {
+  L.marker(e.latlng, { icon: redIcon }).addTo(myMap);
+
+  const { lat, lng } = e.latlng;
+});
